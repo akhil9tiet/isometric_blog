ccf54faaee5960980126a5f8e369a5f73c31f95e
Akhil <akhil9tiet@gmail.com>
2019-02-23 04:36:49 -0800

    pushing the basic layout

--- src/components/isometric-rect.js
+++ src/components/isometric-rect.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './isometric-rect.css';
+import room from '../posts/2019-02-23/room.png';
 
 class IsometricRect extends Component {
   render() {
+    // var posts = 10;
     return (
+      
+
+     
       <div className="grid-link">
-        <h1>get coding</h1>
+        <img src= {room} alt="Tab" />
       </div>
     )
   }
