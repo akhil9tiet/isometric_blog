c04ba0b40e922718d7d818888c34d43f01a30b88
Akhil <akhil9tiet@gmail.com>
2019-02-23 03:26:21 -0800

    styling and content to what will be the isometric  rects

---
+++ src/components/isometric-rect.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './isometric-rect.css';
+
+export default class IsometricRect extends Component {
+  render() {
+    return (
+      <div>
+        <h1>get coding</h1>
+      </div>
+    )
+  }
+}
