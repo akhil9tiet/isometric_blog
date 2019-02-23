8ad603aabe0659a68f8eee679ed2d97d45443781
Akhil <akhil9tiet@gmail.com>
2019-02-23 03:25:59 -0800

    removed css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import IsometricRect from './components/isometric-rect';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>Code this blog</p>
+        <IsometricRect/>
       </div>
     );
   }
