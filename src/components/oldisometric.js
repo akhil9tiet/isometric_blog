8adc996ffc488207ce798f9e9b43147b94d4aedc
Akhil <akhil9tiet@gmail.com>
2019-03-25 01:30:27 -0700

    Created higher order component for grid display

---
+++ src/components/oldisometric.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./isometric-rect.css";
+// import scene from "../posts/2019-02-23/scene.jpg";
+// import image2 from "../posts/2019-02-26/image2.jpg";
+// import bear from "../posts/2019-02-27/bear.jpg";
+// import halo from "../posts/2019-02-28/halo.jpg";
+// import spark from "../posts/2019-03-01/spark.jpg";
+// import img3 from "../posts/2019-03-02/img3.jpg";
+// import img4 from "../posts/2019-03-03/img4.jpg";
+
+const Rect = () => {
+  return(<div></div>)
+};
+
+// class IsometricRect extends Component {
+//   render() {
+//     // var posts = 10;
+//     return (
+//       <div>
+//         <div className="grid-link" id="d1">
+//           <img src={scene} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d2">
+//           <img src={image2} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d3">
+//           <img src={bear} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d4">
+//           <img src={halo} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d5">
+//           <img src={spark} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d6">
+//           <img src={img3} alt="Tab" />
+//         </div>
+//         <div className="grid-link" id="d7">
+//           <img src={img4} alt="Tab" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+export default IsometricRect;
