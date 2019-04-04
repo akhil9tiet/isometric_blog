dc032b626da04c6ee08fabf039f84b8e97e592f9
Akhil <akhil9tiet@gmail.com>
2019-04-03 23:15:47 -0700

    Adding flexbox and height and width as props

--- src/components/isometric-rect.js
+++ src/components/isometric-rect.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types";
+import MasonryLayout from "react-masonry-layout";
 
 // import "./isometric-rect.css";
 import scene from "../posts/2019-02-23/scene.jpg";
@@ -15,30 +15,46 @@ const Rect = props => {
 	console.log(props);
 	var style = {
 		d: {
-			top: props.top * 360,
-			right: props.right * 360
+			// top: props.top * 360,
+			// right: props.right * 360
+			flex: 1,
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "stretch",
+			backgroundColor: "powderblue"
 		}
 	};
 	return (
-		// <div className="grid-link" style={style.d}>
-		<div className="grid-link">
-			<img src={props.img_src} alt={props.alt} />
+		<div className="grid-link" style={style.d}>
+			{/* <div className="grid-link"> */}
+			<img src={props.img_src} alt={props.alt} width={props.width} height={props.height} />
 		</div>
 	);
 };
 
 class IsometricRect extends Component {
+	// state = {
+	// 	perPage: 10,
+	// 	items: Array(90).fill()
+	// };
+
+	// loadItems = () => {
+	// 	this.setState({
+	// 		items: this.state.items.concat(Array(this.state.perPage).fill())
+	// 	});
+	// };
+
 	render() {
 		return (
 			// <div className="isolayer isolayer--scroll1 isolayer--shadow">
 			<div>
-				<Rect img_src={scene} alt="Tab" top={-0.09} right={-0.07} />
-				<Rect img_src={image2} alt="newtab" top={0.31} right={0.6} />
-				<Rect img_src={bear} alt="bear" top={0.7} right={0.18} />
-				<Rect img_src={halo} alt="halo" top={0.54} right={0.41} />
-				<Rect img_src={spark} alt="spark" top={0.18} right={-0.9} />
-				<Rect img_src={img3} alt="img3" top={0.39} right={0.7} />
-				<Rect img_src={img4} alt="img4" top={0.6} right={0.23} />
+				<Rect img_src={scene} alt="Tab" top={-0.09} right={-0.07} width={450} height={300} />
+				<Rect img_src={image2} alt="newtab" top={0.31} right={0.6} width={450} height={300} />
+				<Rect img_src={bear} alt="bear" top={0.7} right={0.18} width={450} height={300} />
+				<Rect img_src={halo} alt="halo" top={0.54} right={0.41} width={450} height={300} />
+				<Rect img_src={spark} alt="spark" top={0.18} right={-0.9} width={450} height={300} />
+				<Rect img_src={img3} alt="img3" top={0.39} right={0.7} width={250} height={300} />
+				<Rect img_src={img4} alt="img4" top={0.6} right={0.23} width={250} height={300} />
 			</div>
 		);
 	}
